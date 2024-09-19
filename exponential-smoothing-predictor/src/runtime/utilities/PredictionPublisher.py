7622c62147e08ca65b0c629f612af86d63c7bdb7
atsag <andreastsagkaropoulos@hotmail.com>
2024-09-19 15:46:56 +0300

    Corrected the publishing of forecasts for a particular application

--- exponential-smoothing-predictor/src/runtime/utilities/PredictionPublisher.py
+++ exponential-smoothing-predictor/src/runtime/utilities/PredictionPublisher.py
@@ -1,11 +1,12 @@
 from exn import core
+from runtime.operational_status.EsPredictorState import EsPredictorState
 
 
 class PredictionPublisher(core.publisher.Publisher):
     metric_name = ""
     def __init__(self,application_name,metric_name):
-        super().__init__('publisher_'+application_name+'-'+metric_name, 'eu.nebulouscloud.preliminary_predicted.'+metric_name, True,True)
+        super().__init__('publisher_'+application_name+'-'+metric_name, 'eu.nebulouscloud.preliminary_predicted.'+EsPredictorState.forecaster_name+"."+metric_name, True,True)
         self.metric_name = metric_name
 
     def send(self, body={}, application=""):
-        super(PredictionPublisher, self).send(body)
+        super(PredictionPublisher, self).send(body, application)
